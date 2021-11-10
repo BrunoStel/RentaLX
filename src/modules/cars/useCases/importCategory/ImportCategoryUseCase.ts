0419257eabfd31828d22b441360a919dcf2cd911
Bruno Stelmastchuk <brunostel@gmail.com>
2021-11-10 10:52:50 -0300

    Implementando rota de importação csv

---
+++ src/modules/cars/useCases/importCategory/ImportCategoryUseCase.ts
@@ -0,0 +1,55 @@
+import csvPaserse from "csv-parse";
+import fs from "fs";
+
+import { CategoriesRepository } from "../../repositories/implementations/CategoriesRepository";
+
+interface IImportCategory {
+    name: string;
+    description: string;
+}
+
+class ImportCategoryUseCase {
+    constructor(private categoriesRepository: CategoriesRepository) {}
+
+    loadCategories(file: Express.Multer.File): Promise<IImportCategory[]> {
+        return new Promise((resolve, reject) => {
+            const stream = fs.createReadStream(file.path);
+
+            const categories: IImportCategory[] = [];
+
+            const parseFile = csvPaserse();
+
+            stream.pipe(parseFile);
+
+            parseFile
+                .on("data", async (line) => {
+                    const [name, description] = line;
+                    categories.push({
+                        name,
+                        description,
+                    });
+                })
+                .on("end", () => {
+                    resolve(categories);
+                })
+                .on("error", (err) => {
+                    reject(err);
+                });
+        });
+    }
+
+    async execute(file: Express.Multer.File): Promise<void> {
+        const categories = await this.loadCategories(file);
+        categories.forEach((obj) => {
+            const { name, description } = obj;
+
+            // eslint-disable-next-line prettier/prettier
+            const nameAlreadyExists = this.categoriesRepository.findByName(name);
+            if (!nameAlreadyExists) {
+                this.categoriesRepository.create({ name, description });
+            }
+        });
+    }
+}
+
+export { ImportCategoryUseCase };
--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,9 +1,17 @@
+/* eslint-disable prettier/prettier */
 import { Router } from "express";
+import multer from "multer";
 
 import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { importCategoryController } from "../modules/cars/useCases/importCategory";
 import { listCategoryController } from "../modules/cars/useCases/listCategories";
 
+
 const categoriesRoutes = Router();
+
+const upload = multer({
+    dest: "./tmp",
+});
 
 categoriesRoutes.post("/", (request, response) => {
     return createCategoryController.handle(request, response);
@@ -13,4 +21,9 @@ categoriesRoutes.get("/", (request, response) => {
     listCategoryController.handle(request, response);
 });
 
+categoriesRoutes.post("/import", upload.single("file"),(request, response) => {
+   return importCategoryController.handle(request, response);
+});
+
 export { categoriesRoutes };
+
